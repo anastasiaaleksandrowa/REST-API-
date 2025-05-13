9057d9ac1200fcb92113bb583380f8b6e384a61e
Kira <kirnan_t@outlok.com>
2025-05-13 21:54:48 +0700

    чтенние данных из файла

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ const app = express();
 app.use(express.json());
 
 const USERS_FILE = path.join(__dirname, 'users.json');
+
+function readUsers() {
+    if (!fs.existsSync(USERS_FILE)) {
+        return [];
+    }
+    const data = fs.readFileSync(USERS_FILE, 'utf8');
+    try {
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Ошибка парсинга JSON:', err);
+        return [];
+    }
+}
